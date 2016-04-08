e072ce6761befa174823a3ee18c3aa3105a997b3
shashi1108 <shashi.raina@nimbo.com>
2016-04-07 22:43:22 -0400

    Added files via upload

---
+++ MainScript.ps1
@@ -0,0 +1,24 @@
+﻿
+Login-AzureRmAccount -SubscriptionId  f794c958-ef55-4f8c-834b-a90d282fb3bd 
+#Remove-AzureRmResourceGroup -Name bootcampnyc
+
+New-AzureRmResourceGroup -Name sr-bootcamp -Location 'East US' -Force
+
+$Deployment = @{
+	Name='shashideployment';
+	ResourceGroupName='sr-bootcamp';
+	Mode='Complete';
+	#TemplateFile="$PSScriptRoot\azure.json";
+	TemplateFile="C:\projects\AzureBootCampV1\DeploymentScript\azure-C.json";
+	TemplateParameterObject =@{
+		StorageAccountName='srbootcamp';
+		adminUsername='shashi';
+		adminPassword='Raina123!';
+		dnsNameForPublicIP1='sr-bootcamp1';
+		dnsNameForPublicIP2='sr-bootcamp2';
+		dnsNameforLBIP= 'sr-bootcamplb';
+	}
+
+}
+New-AzureRmResourceGroupDeployment  @Deployment;
+
