c5a9a1bbb16f21b1db922d49c3879b2dcaab4a7b
shashi1108 <shashi.raina@nimbo.com>
2016-04-07 22:29:46 -0400

    Update MainScript.ps1

--- MainScript.ps1
+++ MainScript.ps1
@@ -1,5 +1,5 @@
 
-Login-AzureRmAccount -SubscriptionId  f794c958-ef55-4f8c-834b-a90d282fb3bd 
+Login-AzureRmAccount -SubscriptionId  [SUB ID]
 #Remove-AzureRmResourceGroup -Name bootcampnyc
 
 New-AzureRmResourceGroup -Name sr-bootcamp -Location 'East US' -Force
@@ -13,7 +13,7 @@ $Deployment = @{
 	TemplateParameterObject =@{
 		StorageAccountName='srbootcamp';
 		adminUsername='shashi';
-		adminPassword='Raina123!';
+		adminPassword='Raina123!'; # 1 UPPER CASE, SPECIAL CHARACTER, NUMBER AND min 8 character lenght
 		dnsNameForPublicIP1='sr-bootcamp1';
 		dnsNameForPublicIP2='sr-bootcamp2';
 		dnsNameforLBIP= 'sr-bootcamplb';
